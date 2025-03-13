1cc40b59735b409b862267d6ce00f7dc7a11e6de
Mugdha Adhav <mugdha.adhav@gmail.com>
2025-03-13 20:36:06 +0530

    Implement basic login and registration

--- backend/main.go
+++ backend/main.go
@@ -9,6 +9,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type User struct {
@@ -16,7 +17,8 @@ type User struct {
 
 	ID       uuid.UUID `gorm:"primaryKey"`
 	Name     string    `gorm:"type:varchar(40);unique" json:"name,omitempty"`
-	Password string    `gorm:"size:255" json:"password,omitempty"`
+	Password string    `gorm:"size:255" json:"password"`
+	Email    string    `gorm:"type:varchar(40);unique" json:"email"`
 }
 
 // makePasswordHash generates a password hash
@@ -46,9 +48,10 @@ func dbInit() (*gorm.DB, error) {
 			return nil, fmt.Errorf("failed to seed user: %w", err)
 		}
 
-		if err := db.FirstOrCreate(&User{
+		if err := db.Clauses(clause.OnConflict{DoNothing: true}).Create(&User{
 			ID:       uuid.New(),
 			Name:     "defaultUser",
+			Email:    "default@example.com",
 			Password: hashedPassword,
 		}).Error; err != nil {
 			return nil, fmt.Errorf("failed to seed user: %w", err)
@@ -70,16 +73,30 @@ func main() {
 			"message": "pong",
 		})
 	})
+
 	r.POST("/login", func(c *gin.Context) {
 		var user User
 		if c.ShouldBind(&user) == nil {
 			var dbUser User
-			result := db.Where("name = ?", user.Name).First(&dbUser)
-			if result.Error != nil {
-				c.JSON(401, gin.H{
-					"message": "User not found",
-				})
-				return
+
+			if user.Name != "" {
+				result := db.Where("name = ?", user.Name).First(&dbUser)
+				if result.Error != nil {
+					c.JSON(401, gin.H{
+						"message": "User not found",
+					})
+					return
+				}
+			}
+
+			if user.Email != "" {
+				result := db.Where("email = ?", user.Email).First(&dbUser)
+				if result.Error != nil {
+					c.JSON(401, gin.H{
+						"message": "Email not found",
+					})
+					return
+				}
 			}
 
 			err := bcrypt.CompareHashAndPassword([]byte(dbUser.Password), []byte(user.Password))
@@ -92,6 +109,58 @@ func main() {
 
 			c.JSON(200, gin.H{
 				"message": "Login successful",
+			})
+		}
+	})
+
+	r.POST("/register", func(c *gin.Context) {
+		var user User
+		if c.ShouldBind(&user) == nil {
+			var dbUser User
+
+			if user.Password == "" || user.Email == "" || user.Name == "" {
+				c.JSON(401, gin.H{
+					"message": "Please enter all details",
+				})
+				return
+			}
+
+			result := db.Where("email = ?", user.Email).First(&dbUser)
+			if result.Error == nil {
+				c.JSON(401, gin.H{
+					"message": "Email already exists",
+				})
+				return
+			}
+
+			result = db.Where("name = ?", user.Name).First(&dbUser)
+			if result.Error == nil {
+				c.JSON(401, gin.H{
+					"message": "Name already exists",
+				})
+				return
+			}
+
+			pass, err := makePasswordHash(user.Password)
+			if err != nil {
+				c.JSON(401, gin.H{
+					"message": "Failed to register user",
+				})
+			}
+
+			if err := db.Create(&User{
+				ID:       uuid.New(),
+				Name:     user.Name,
+				Email:    user.Email,
+				Password: pass,
+			}).Error; err != nil {
+				c.JSON(401, gin.H{
+					"message": "Failed to register user",
+				})
+			}
+
+			c.JSON(200, gin.H{
+				"message": "Registration successful",
 			})
 		}
 	})
